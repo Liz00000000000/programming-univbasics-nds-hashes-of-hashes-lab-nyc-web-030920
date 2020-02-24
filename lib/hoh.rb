380161826d5f851c5ae5dac27cff9ce61800454c
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-24 23:41:54 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -3,32 +3,32 @@
 # that HoH as the return value for the method "naming_system."
 #
 # The tests will guide your construction.
-#
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-#
-# PORTION_2 = {
-#   label: "Order"
-# }
-#
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-#
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+
+ PORTION_1 = {
+   label: "Kingdom",
+  sub_category: {
+     label: "Phylum",
+     sub_category: {
+       label: "Class"
+    }
+   }
+ }
+
+ PORTION_2 = {
+   label: "Order"
+ }
+
+ PORTION_3 = {
+   label: "Family",
+   sub_category: {
+     label: "Genus",
+   }
+ }
+
+ PORTION_4 = {
+   label: "Species",
+   sub_category: nil
+}
 
 Hash = { }
 
@@ -45,8 +45,7 @@ def naming_system
   # HoH all the way to Species!
  PORTION_1[:sub_category][:sub_category][:sub_category] = (PORTION_2)
  PORTION_1
- PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category]
- = PORTION_3
+ PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
  PORTION_1
  PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category]
  [:sub_category][:sub_category] = PORTION_4
